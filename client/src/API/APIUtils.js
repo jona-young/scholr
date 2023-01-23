24ef3828b46ea15efda411d7b1a6dda40da937e8
jonayoung <young.jon@icloud.com>
2023-01-23 17:48:20 -0500

    added modular API functions, skeleton general layout

---
+++ client/src/API/APIUtils.js
@@ -0,0 +1,91 @@
+// send POST request to backend api to store record
+export const fetchPOST = async (apiRoute, form) => {
+    const data = await fetch(process.env.REACT_APP_API + apiRoute, {
+        credentials: 'include',
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(form)
+    })
+
+    const json = await data.json();
+
+    if (json.errors)
+    {
+        return json.errors;
+    }
+
+    return json;
+}
+
+// send GET request to backend api to gather records
+export const fetchGET = async (apiRoute) => {
+    const data = await fetch(process.env.REACT_APP_API + apiRoute, {
+        credentials: 'include',
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'},
+    })
+
+    const json = await data.json();
+
+    if (json.errors)
+    {
+        return json.errors;
+    }
+
+    return json;
+}
+
+// send GET request to backend api to gather single record
+export const fetchGETSINGLE = async (apiRoute, id) => {
+    const data = await fetch(process.env.REACT_APP_API + apiRoute + id, {
+        credentials: 'include',
+        method: 'GET',
+        headers: {'Content-Type': 'application/json',}
+    })
+
+    const json = await data.json();
+
+    if (json.errors)
+    {
+        return json.errors;
+    }
+
+    return json;
+}
+
+// send PUT request to backend api to update single record
+export const fetchPUT = async (apiRoute, form) => {
+    const data = await fetch(process.env.REACT_APP_API + apiRoute, {
+        credentials: 'include',
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON>stringify(form)
+    })
+
+    const json = await data.json();
+
+    if (json.errors)
+    {
+        return json.errors;
+    }
+
+    return json;
+}
+
+// send DELETE request to backend api to delete single record
+export const fetchDELETE = async (apiRoute, id) => {
+    const data = await fetch(process.env.REACT_APP_API + apiRoute + "/" + id, {
+        credentials: 'include',
+        method: 'DELETE',
+        headers: {'Content-Type': 'application/json'},
+    })
+
+    const json = await data.json();
+
+    if (json.errors)
+    {
+        return json.errors;
+    }
+
+    return json;
+}
--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Layouts/Header.js';
+import SideMenu from './Layouts/SideMenu.js';
+import Footer from './Layouts/Footer.js';
+
+
 function App() {
+
+  // implement routes here <Route path="urlPath" element={<componentName />} />
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <BrowserRouter>
+          <Header />
+          <SideMenu />
+          <Routes>
+
+          </Routes>
+          <Footer />
+      </BrowserRouter>
     </div>
   );
 }
